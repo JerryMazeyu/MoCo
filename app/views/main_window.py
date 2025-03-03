5ac682c3958ee8f40fed37ee74f50c165c15acb1
JerryMazeyu <jerry_mazeyu@163.com>
2025-03-03 22:07:01 +0800

    Modify: fix flow5 bug

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -32,7 +32,6 @@ class MainWindow(QMainWindow):
         # Add tabs to the tab widget
         self.tab_widget.addTab(self.tab5, "城市餐厅信息爬取")
         self.tab_widget.addTab(self.tab0, "配置项")
-        self.tab_widget.addTab(self.tab5, "城市餐厅信息爬取")
         self.tab_widget.addTab(self.tab1, "查找&确认餐厅信息")
         self.tab_widget.addTab(self.tab2, "配置车辆信息")
         self.tab_widget.addTab(self.tab3, "生成&审核")
@@ -45,7 +44,6 @@ class MainWindow(QMainWindow):
         self.tab1.layout = QVBoxLayout()
         self.tab2.layout = QVBoxLayout()
         self.tab3.layout = QVBoxLayout()
-        self.tab5.layout = QVBoxLayout()
         self.tab6.layout = QVBoxLayout()
 
 
@@ -58,8 +56,6 @@ class MainWindow(QMainWindow):
         self.tab1.layout.addWidget(self.tab1_content)
         self.tab2_content = Tab2()
         self.tab2.layout.addWidget(self.tab2_content)
-        self.tab5_content = Tab5()
-        self.tab5.layout.addWidget(self.tab5_content)
         self.tab6_content = Tab6()
         self.tab6.layout.addWidget(self.tab6_content)
 
@@ -68,7 +64,6 @@ class MainWindow(QMainWindow):
         self.tab1.setLayout(self.tab1.layout)
         self.tab2.setLayout(self.tab2.layout)
         self.tab3.setLayout(self.tab3.layout)
-        self.tab5.setLayout(self.tab5.layout)
         self.tab6.setLayout(self.tab6.layout)
 
 if __name__ == "__main__":
